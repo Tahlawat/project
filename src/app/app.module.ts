a97e1ae8e36423c0cba0901ddc4f4f5985d59a2a
Tahlawat <105737513+Tahlawat@users.noreply.github.com>
2023-08-05 16:17:25 +0530

    Navbar and all

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,45 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { MatButtonModule } from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import{MatMenuModule} from'@angular/material/menu';
+import { CarouselComponent } from './Home/carousel/carousel.component';
+import { HomeComponent } from './Home/home/home.component';
+import { EveryVoiceComponent } from './Home/every-voice/every-voice.component';
+import { FeaturesComponent } from './Home/features/features.component';
+import { TutorialComponent } from './Home/tutorial/tutorial.component';
+import { PricingComponent } from './Home/pricing/pricing.component';
+import { PicboardComponent } from './PictureBoard/picboard/picboard.component';
+import { EmotionsComponent } from './PictureBoard/emotions/emotions.component';
+import { GroceryComponent } from './PictureBoard/grocery/grocery.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    HomeComponent,
+    CarouselComponent,
+    EveryVoiceComponent,
+    FeaturesComponent,
+    TutorialComponent,
+    PricingComponent,
+    PicboardComponent,
+    EmotionsComponent,
+    GroceryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
